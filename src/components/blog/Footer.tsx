0a8556bbb31aa3838bfe412be9786a563de23f7e
Vincent Seube <107034608+vin-dow-s@users.noreply.github.com>
2025-02-15 15:27:00 +0100

    Footer accessibility

--- src/components/blog/Footer.tsx
+++ src/components/blog/Footer.tsx
@@ -2,31 +2,10 @@ import Link from 'next/link'
 
 export const Footer = () => {
     return (
-        <footer className="mt-4 flex items-center rounded-sm p-2">
-            <div className="container mx-auto text-center text-xs">
-                <p>
-                    &copy; {new Date().getFullYear()} Megan's Page. All rights
-                    reserved.
-                </p>
-
-                <nav aria-label="Footer navigation" className="text-xs">
-                    <div className="flex justify-center gap-4">
-                        <Link
-                            href="/privacy-policy"
-                            className="border-none bg-transparent bg-none font-light hover:underline"
-                        >
-                            Privacy Policy
-                        </Link>{' '}
-                        -
-                        <Link
-                            href="/terms"
-                            className="border-none bg-transparent bg-none font-light hover:underline"
-                        >
-                            Terms of Use
-                        </Link>
-                    </div>
-                </nav>
-                <p className="text-xs">
+        <footer className="mt-4 flex items-center rounded-sm">
+            <div className="container mx-auto text-center text-sm">
+                <p className="mb-2">
+                    &copy; {new Date().getFullYear()} Megan's Page.{' '}
                     <a
                         href="mailto:contact@vindows.dev"
                         className="border-none bg-transparent bg-none font-normal hover:underline"
@@ -34,6 +13,22 @@ export const Footer = () => {
                         Developed by Vincent S.
                     </a>
                 </p>
+                <nav aria-label="Footer navigation">
+                    <div className="flex items-center justify-center gap-6 text-xs">
+                        <Link
+                            href="/privacy-policy"
+                            className="border-none bg-transparent bg-none p-2 font-light hover:underline"
+                        >
+                            Privacy Policy
+                        </Link>{' '}
+                        <Link
+                            href="/terms"
+                            className="border-none bg-transparent bg-none p-2 font-light hover:underline"
+                        >
+                            Terms of Use
+                        </Link>
+                    </div>
+                </nav>
             </div>
         </footer>
     )
